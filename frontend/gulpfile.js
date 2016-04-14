9800b909b791612f52012eb38c7e1fd41fa0537b
wrongthings <grace950616@163.com>
2016-04-14 22:08:52 +0800

    debug the livereload

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,62 +2,76 @@ var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var watch = require('gulp-watch');
 var batch = require('gulp-batch');
-var connect = require('gulp-connect');
+//var connect = require('gulp-connect');
 var copy = require('gulp-copy');
 var runSeq = require('run-sequence');
+//var watch = require('gulp-watch');
+var server = require('gulp-server-livereload');
 
 // Run webpack
-gulp.task('webpack', function(){
+gulp.task('webpack', function() {
   gulp.src('src/customer/main.js')
     .pipe(webpack( require('./customer_webpack.config.js') ))
     .pipe(gulp.dest('dist/customer/js/'))
-    .pipe(connect.reload());
   gulp.src('src/bussiness/main.js')
     .pipe(webpack( require('./bussiness_webpack.config.js') ))
     .pipe(gulp.dest('dist/bussiness/js/'))
-    .pipe(connect.reload());
   gulp.src('src/admin/main.js')
     .pipe(webpack( require('./admin_webpack.config.js') ))
     .pipe(gulp.dest('dist/admin/js/'))
-    .pipe(connect.reload());
 });
 
 // Run the webserver
 gulp.task('webserver_customer', function() {
-  connect.server({
-    port: 8080,
-    livereload: {port: 35750},
-    root: 'dist/customer'
-  });
+  gulp.src('dist/customer')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        port: 35729
+      },
+      port: 8080
+    }));
 });
 
 gulp.task('webserver_bussiness', function() {
-  connect.server({
-    port: 8081,
-    livereload: {port: 35751},
-    root: 'dist/bussiness'
-  });
+  gulp.src('dist/bussiness')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        port: 35730
+      },
+      port: 8081
+    }));
 });
 
 gulp.task('webserver_admin', function() {
-  connect.server({
-    port: 8082,
-    livereload: {port: 35752},
-    root: 'dist/admin'
-  });
+  gulp.src('dist/admin')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        port: 35731
+      },
+      port: 8082
+    }));
 });
 
 // Copy index.html file
 gulp.task('build.index', function(){
-  gulp.src('./src/customer/index.html')
-    .pipe(gulp.dest('./dist/customer/'));
-  gulp.src('./src/bussiness/index.html')
-    .pipe(gulp.dest('./dist/bussiness/'));
-  gulp.src('./src/admin/index.html')
-    .pipe(gulp.dest('./dist/admin/'));
+  gulp.src('src/customer/index.html')
+    .pipe(gulp.dest('dist/customer/'));
+  gulp.src('src/bussiness/index.html')
+    .pipe(gulp.dest('dist/bussiness/'));
+  gulp.src('src/admin/index.html')
+    .pipe(gulp.dest('dist/admin/'))
+});
+
+// Watch changes in index.html file && main.js file
+gulp.task('watch', function() {
+  gulp.watch(['src/**/index.html'], ['build.index']);
+  gulp.watch(['src/**/main.js'], ['webpack']);
 });
 
 // Default task
 gulp.task('default', function(callback) {
-    runSeq('webserver_customer', 'webserver_bussiness', 'webserver_admin', ['webpack', 'build.index'], callback);
+  runSeq(['webpack', 'build.index', 'watch'], 'webserver_customer', 'webserver_bussiness', 'webserver_admin');
 });
