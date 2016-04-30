e974730fac410898d6de4c93175c1ed952257c63
wrongthings <grace950616@163.com>
2016-04-30 18:35:40 +0800

    查看商家详细菜单

--- frontend/src/customer/components/Home.vue
+++ frontend/src/customer/components/Home.vue
@@ -17,11 +17,13 @@
 
 <template>
 <div>
-  <sortbar :sortpara.sync="sortpara"></sortbar>
+  <div class="row small-up-1 medium-up-2 large-up-4">
+    <sortbar :sortpara.sync="sortpara"></sortbar>
+  </div>
   <div class="row small-up-1 medium-up-2 large-up-4" id="customer-home">
     <template v-for="item in business | filterBy searchText | orderBy sortpara -1">
       <div class="column">
-        <a v-bind:href="item.myurl">
+        <a v-link="{name: 'detail', params: {bussinessId: item.id}}">
           <div class="row customer-item" small-2>
             <div class="column">
               <img :src="item.picture" />
